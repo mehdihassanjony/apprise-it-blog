6a54f2d45cef26f3e86083d6678a5f10e20b5187
Mehdi Hassan Jony <mehdi.jony@loopfreight.io>
2023-04-04 15:54:16 +0600

    first commit

---
+++ src/config/database.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const DATABASE_CONFIG: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: '127.0.0.1',
+  port: 5432, // * use 3306 for mySQL,
+  username: 'local',
+  password: 'local',
+  database: 'nest_blog',
+  entities: ['src/**/*.entity{.ts,.js}'],
+  synchronize: true, // TODO: switch to false in prod & use migrations instead !
+
+  migrationsTableName: 'migrations',
+  migrations: ['migration/*.js'],
+  // cli: { migrationsDir: 'migration' },
+};
